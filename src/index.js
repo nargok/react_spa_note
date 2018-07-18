741013a77ce974580732cc48a692ca60c9ac78fa
Ryoichi Gokan <nargok@gmail.com>
2018-07-18 22:37:23 +0900

    create teamplate component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class SpaNote extends Component {
+  render() {
+    return (
+      <div>Hello SPA!</div>
+    )
+  }
+}
+
+ReactDOM.render(<SpaNote />, document.getElementById('root'));
