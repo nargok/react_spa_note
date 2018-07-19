88cf3b788d12d851fcd16dd0f45a7364da538219
Ryoichi Gokan <nargok@gmail.com>
2018-07-20 00:51:15 +0900

    create GlobalHeader component

---
+++ src/components/GlobalHeader/GlobalHeader.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router'
+
+class GlobalHeader extends Component {
+  render() {
+    return(
+      <div className="GlobalHeader">
+        <div className="GlobalHeader-inner">
+          <h1 className="GlobalHeader-title"><Link to="/">SPA Note</Link></h1>
+          <div className="GlobalHeader-menu">
+            <span className="GlobalHeader-user">
+              <img src="/assets/user.svg" width="16" height="16" />
+              <span>MyUserName</span>
+            </span>
+            <Link to="/starred" className="GlobalHeader-menuItem">Starred</Link>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default GlobalHeader
